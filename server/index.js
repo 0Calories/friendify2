e417e8e52f90c58bee3fed975fb3390c01cf6a38
Ash Anand <cppanand@gmail.com>
2019-09-14 11:36:49 -0400

    Add initial Firebase setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
+const admin = require('firebase-admin');
+const QRCode = require('qrcode');
 const express = require('express');
 const app = express();
 const port = 4200;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const serviceAccount = require('./auth.json');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://hack-the-north-2019-97ff8.firebaseio.com'
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// POST /users/create
+app.post('/users/create', (req, res) => {
+  
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
