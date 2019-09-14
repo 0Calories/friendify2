139cc57fc892ea9b5e1d6d9ccc2d0fe9327603f4
Ash Anand <cppanand@gmail.com>
2019-09-14 16:45:55 -0400

    Fixing AR issue

--- client/friendify/src/components/UserAr/index.js
+++ client/friendify/src/components/UserAr/index.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 
 class UserAr extends Component {
 
+  componentDidMount() {
+    document.body.innerHTML = createMarkUp();
+  }
+
   render() {
     return (
-      <div dangerouslySetInnerHTML={createMarkUp()}></div>
+      <p>test</p>
+      // <div dangerouslySetInnerHTML={createMarkUp()}></div>
     );
   }
 }
 
 function createMarkUp() {
-  return {__html: '<a-scene embedded arjs><a-marker preset="hiro"><a-box position="0 0.5 0" material="color: yellow;"></a-box></a-marker><a-entity camera></a-entity></a-scene>'}
+  return '<a-scene embedded arjs><a-marker preset="hiro"><a-box position="0 0.5 0" material="color: yellow;"></a-box></a-marker><a-entity camera></a-entity></a-scene>'
 }
 
 export default UserAr
