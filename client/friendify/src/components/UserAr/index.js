63bf68a7d644dedcde0c5a0eb1d9e586497935cc
Ash Anand <cppanand@gmail.com>
2019-09-14 17:17:25 -0400

    Modify shape of AR object

--- client/friendify/src/components/UserAr/index.js
+++ client/friendify/src/components/UserAr/index.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 
 class UserAr extends Component {
 
-  componentDidMount() {
-    document.body.innerHTML = createMarkUp();
-  }
-
-  render() {
+  getArRendering = () => {
     return (
-      <p>test</p>
-      // <div dangerouslySetInnerHTML={createMarkUp()}></div>
-    );
+      <a-scene embedded arjs>
+        <a-marker preset="hiro">
+          <a-box position="0 0.5 0" material="color: blue;" depth="0.2" height="10" width="5">
+          </a-box>
+        </a-marker>
+        <a-entity camera></a-entity>
+      </a-scene>
+    )
+  }
+
+  componentDidMount() {
+    document.body.innerHTML = this.getArRendering();
   }
 }
 
-function createMarkUp() {
-  return '<a-scene embedded arjs><a-marker preset="hiro"><a-box position="0 0.5 0" material="color: yellow;"></a-box></a-marker><a-entity camera></a-entity></a-scene>'
-}
+
 
 export default UserAr
