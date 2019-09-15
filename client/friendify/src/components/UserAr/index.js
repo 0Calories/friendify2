0bbd018f130a469bb9b0caab3ecf003e20c9e90b
Ash Anand <cppanand@gmail.com>
2019-09-14 23:10:54 -0400

    Add button placeholder

--- client/friendify/src/components/UserAr/index.js
+++ client/friendify/src/components/UserAr/index.js
@@ -22,6 +22,7 @@ class UserAr extends Component {
           <a-plane position="0 0 0" rotation="-90 0 0" width="3" height="5" color="white" shadow material="shader: flat;">
             <a-entity position="1.5 1.5 0.1" text="width: 5; color: black; value: Hello world!"></a-entity>
             <a-entity position="-1 0 0" geometry="primitive: plane; height: 1; width: 1" material="shader: flat; src: #chung"></a-entity>
+            <a-entity position="-1 0 0" geometry="primitive: plane; height: 0.5; width: 1" material="shader: flat; color: #4167B2"></a-entity>
           </a-plane>
 
         </a-marker>
